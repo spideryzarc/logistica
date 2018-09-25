8a917620adeeaa117a141e3fda2fec256893ec0f
spideryzarc <spideryzarc@gmail.com>
2018-09-25 15:35:01 -0300

    modelo para o PCV, com eliminação dos subciclos

--- PCV.py
+++ PCV.py
@@ -75,26 +75,53 @@ def createPyomoModel(pts):
     C = dist(pts)
     Cdic = matrixToDic(C)
     model = env.ConcreteModel()
+    model.pts = pts
     model.name = 'PCV'
     model.V = env.Set(initialize=range(len(pts)),doc='conjunto dos vertices')
+    model.Vs0= env.Set(initialize=range(1,len(pts)), doc='conjunto dos vertices sem a origem')
     model.x = env.Var(model.V,model.V,within=env.Binary,doc='se o arco (i,j) eh usado ou nao')
     model.c = env.Param(model.V,model.V,initialize=Cdic,doc='custo do arco (i,j)')
 
-    model.objective = env.Objective(rule=lambda model: sum(model.x[i,j]*model.c[i,j] for i in model.V for j in model.V if i!=j)
+    model.objective = env.Objective(rule=lambda model: sum(model.x[i,j]*model.c[i,j]
+                                                           for i in model.V for j in model.V if i!=j)
                                      ,sense=env.minimize)
 
-    model.leaving = env.Constraint(model.V,rule=lambda model,i:sum(model.x[i,j] for j in model.V if i!=j) ==1)
-    model.incoming = env.Constraint(model.V, rule=lambda model, j: sum(model.x[i,j] for i in model.V if i != j) == 1)
+    model.leaving = env.Constraint(model.Vs0,
+                                   rule=lambda model,i:sum(model.x[i,j] for j in model.V if i!=j) ==1)
+    model.incoming = env.Constraint(model.Vs0,
+                                    rule=lambda model, j: sum(model.x[i,j] for i in model.V if i != j) == 1)
 
+    #remocao dos subcirclos
+    model.y = env.Var(model.V, model.V, within=env.NonNegativeIntegers, doc='qtd. de comodities trans. de (i,j)')
+    N = len(pts)
+    model.c1 = env.Constraint(expr=sum(model.y[0,i] for i in model.V) == N, doc='N comodities saindo da origem')
+    model.c2 = env.Constraint(expr=sum(model.y[i,0] for i in model.V) == 1, doc='1 comodite volta para a origem')
+    model.c3 = env.Constraint(model.V,model.V,rule=lambda model,i,j:model.y[i,j]<=N*model.x[i,j])
 
+    model.c4 = env.Constraint(model.Vs0,
+                              rule=lambda model,j:sum(model.y[i,j] for i in model.V if (i!=j))
+                                                  - sum(model.y[j,i] for i in model.V if (i!=j)) == 1 )
+    #model.c5 = env.Constraint(model.V, rule=lambda model, i: model.y[0, i] <= 3)
     return model
 
 
+def plot(model):
+    pts = model.pts
+    x = [p[0] for p in pts]
+    y = [p[1] for p in pts]
+    plt.close()
+    plt.plot(x, y, 'ro')
+    for i in range(len(pts)):
+        for j in range(len(pts)):
+            if model.x[i,j].value >= 1:
+                plt.plot([x[i],x[j]], [y[i],y[j]], '-')
+    plt.show()
 
-model = createPyomoModel(createRandomInstance(5))
+
+model = createPyomoModel(createRandomInstance(15))
 solveWithGLPK(model)
 model.display()
-model.x[0,0].value
+plot(model)
 
 
 # pts = createRandomInstance(50)
